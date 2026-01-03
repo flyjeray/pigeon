69486c051835657e12fb89fccd98deb2d8dbf36a
flyjeray <flyjeray@gmail.com>
2026-01-04 01:58:06 +0300

    chore: intentionally break testing for deploy cancel

--- pigeon-clientside-encryption/test/encryption.test.ts
+++ pigeon-clientside-encryption/test/encryption.test.ts
@@ -116,7 +116,7 @@ describe("private key encryption", () => {
       recipe
     );
 
-    expect(decrypted).toBe(privateKey);
+    expect(decrypted).toBe(privateKey + "typo");
   });
 
   it("rejects decryption with wrong passphrase", async () => {
