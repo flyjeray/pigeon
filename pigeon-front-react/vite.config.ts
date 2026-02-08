bb06fb9ac082d8df0d5f012bb15bd83868a0f867
flyjeray <flyjeray@gmail.com>
2026-02-08 15:07:09 +0300

    chore: use VITE_URL_BASE instead of hardcoded base URL

--- pigeon-front-react/vite.config.ts
+++ pigeon-front-react/vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/pigeon/",
-  plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: "camelCase",
+export default ({ mode }: { mode: string }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    base: process.env.VITE_URL_BASE || "/",
+    plugins: [react()],
+    css: {
+      modules: {
+        localsConvention: "camelCase",
+      },
     },
-  },
-});
+  });
+};
